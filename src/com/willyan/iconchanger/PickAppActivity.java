574c47494792dc95e6c3a0b3df26e3b9abafc242
yweiquan <willyan0820@gmail.com>
2013-06-28 01:22:27 +0800

    define layout style

--- src/com/willyan/iconchanger/PickAppActivity.java
+++ src/com/willyan/iconchanger/PickAppActivity.java
@@ -43,7 +43,6 @@ public class PickAppActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_pickapp);
 		listView = (ListView) findViewById(R.id.applist);
 		manager = getPackageManager();
@@ -67,13 +66,6 @@ public class PickAppActivity extends Activity {
 			}
 		});
 		loadAppList();
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
 	}
 	
 	/**
