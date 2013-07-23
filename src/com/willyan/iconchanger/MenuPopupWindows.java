06b3e10edf00d6bb6f88ecf0f1667713613667e9
WillYan <willyan0820@gmail.com>
2013-07-23 18:00:04 +0800

    Add PopupWindows

---
+++ src/com/willyan/iconchanger/MenuPopupWindows.java
@@ -0,0 +1,20 @@
+package com.willyan.iconchanger;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.PopupWindow;
+
+public class MenuPopupWindows {
+	
+	private Activity mActivity;
+	private PopupWindow mPopWindow=null;
+
+	public MenuPopupWindows(View attchView,Activity activity) {
+		mActivity = activity;
+		initView();
+	}
+	
+	private void initView(){
+		//TODO
+	}
+}
