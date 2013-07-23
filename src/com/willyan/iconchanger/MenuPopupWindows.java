75ce5002404269250bbf8a75a6ecb78d26408d6d
WillYan <willyan0820@gmail.com>
2013-07-23 18:20:36 +0800

    Popupwindow layout

--- src/com/willyan/iconchanger/MenuPopupWindows.java
+++ src/com/willyan/iconchanger/MenuPopupWindows.java
@@ -1,6 +1,7 @@
 package com.willyan.iconchanger;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.PopupWindow;
 
@@ -8,13 +9,29 @@ public class MenuPopupWindows {
 	
 	private Activity mActivity;
 	private PopupWindow mPopWindow=null;
+	private View mAttchView;
 
 	public MenuPopupWindows(View attchView,Activity activity) {
 		mActivity = activity;
+		mAttchView = attchView;
 		initView();
 	}
 	
 	private void initView(){
+		
+		View contentView = LayoutInflater.from(mActivity).inflate(R.layout.popup_menu, null);
+		mPopWindow = new PopupWindow(contentView);
+		mPopWindow.setOutsideTouchable(true);
 		//TODO
+	}
+	
+	public void show(){
+		//TODO
+		mPopWindow.showAsDropDown(mAttchView);
+	}
+	
+	public void dismiss(){
+		//TODO
+		mPopWindow.dismiss();
 	}
 }
