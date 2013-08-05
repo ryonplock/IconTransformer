27c6bdb83cfca4580abd3d52a64bf00e4dc56962
WillYan <willyan0820@gmail.com>
2013-08-05 18:22:35 +0800

    Setting Activity

--- src/com/willyan/iconchanger/SettingActivity.java
+++ src/com/willyan/iconchanger/SettingActivity.java
@@ -2,13 +2,20 @@ package com.willyan.iconchanger;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingActivity extends Activity{
+	
+	private Button btn_menu;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		setContentView(R.layout.activity_setting);
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_setting);
+		
+		btn_menu = (Button) findViewById(R.id.menu);
+		btn_menu.setVisibility(View.GONE);
 	}
 
 }
