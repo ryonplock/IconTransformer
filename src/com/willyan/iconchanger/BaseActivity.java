9283965757f98b18ad4cf1b48c756f190d045538
WillYan <willyan0820@gmail.com>
2013-08-08 16:55:18 +0800

    add umeng sdk

---
+++ src/com/willyan/iconchanger/BaseActivity.java
@@ -0,0 +1,32 @@
+package com.willyan.iconchanger;
+
+import com.umeng.analytics.MobclickAgent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BaseActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		MobclickAgent.onResume(this);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		MobclickAgent.onPause(this);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+}
